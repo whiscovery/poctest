e4652a7dbd92bfd58ab4d61dc29a7bfdd520a4e1
whiscovery <whiscovery@gmail.com>
2020-10-03 22:48:24 +0900

    ?

--- src/components/SpeakersByRank.vue
+++ src/components/SpeakersByRank.vue
@@ -6,11 +6,13 @@
         :key="index"
         class="p-2 mr-auto justify-content-center bd-hightlight"
       >
-        <div class="card" style="width: 240px">
-          <div class="img-box text-center">
-            <img src="" width="212" />
+        <div v-if="show">
+          <div class="card" style="width: 240px">
+            <div class="img-box text-center">
+              <img src="" width="212" />
+            </div>
+            {{ speakers.name }}
           </div>
-          {{ speakers.name }}
         </div>
       </div>
     </div>
@@ -18,15 +20,48 @@
 </template>
 
 <script>
+import axios from "axios"
 export default {
-  props: ["speakersInfo"],
   data() {
     return {
-      speakerName: null
+      i: null,
+      show: true,
+      speakersInfo: [],
+      speakers: []
     }
   },
+  created() {
+    this.loadSpeakers()
+  },
+  // mounted() {
+  //   this.printSpeakers()
+  // },
   methods: {
-    printSpeakers() {}
+    async loadSpeakers() {
+      await axios
+        .get(
+          "https://script.googleusercontent.com/macros/echo?user_content_key=JkcOfggJobXcP2YBrYDmVOshdXuQJbfRn0-XfdfZhVChBNWuHoXrqXg9kiAMWWj4cGsncd2PqRRugogEOMs-9iKSCiARl4Nrm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnOqxU5QC_nZgRkPHyeCNP-n4QIUB6fjPh83U0hwv1lFRVFgZq68P2O7iBaYhCunJ0JK7g-6hpKLuKFxq41JwiLj3YGXp3BxfjQ&lib=MXlEvNmo98u7JPl4wcD1WZM7W5hCA46zX"
+        )
+        .then(res => {
+          // handle success
+          this.speakersInfo = res.data.data
+        })
+        .catch(err => {
+          // handle error
+          console.log(err)
+        })
+        .then(() => {
+          // always executed
+        })
+    },
+    numberOfSpeech() {}
+    // makeNewArray() {
+    //   for (i = 0; i < this.speakersInfo.length - 1; i++) {
+    //     if (this.speakersInfo[i]["name"] == this.speakersInfo[i + 1]["name"]) {
+    //       this.speakers.push(this.speakersInfo[i])
+    //     }
+    //   }
+    // }
   }
 }
 </script>
--- src/views/Speaker.vue
+++ src/views/Speaker.vue
@@ -1,14 +1,11 @@
 <template>
   <v-container>
-    <SpeakersByRankComponent
-      :speakersInfo="speakersInfo"
-    ></SpeakersByRankComponent>
+    <SpeakersByRankComponent></SpeakersByRankComponent>
   </v-container>
 </template>
 
 <script>
 import SpeakersByRankComponent from "@/components/SpeakersByRank.vue"
-import axios from "axios"
 
 export default {
   components: {
@@ -16,29 +13,8 @@ export default {
   },
   data() {
     return {
-      speakersInfo: {}
-    }
-  },
-  mounted() {
-    this.loadSpeakers()
-  },
-  methods: {
-    async loadSpeakers() {
-      await axios
-        .get(
-          "https://script.googleusercontent.com/macros/echo?user_content_key=JkcOfggJobXcP2YBrYDmVOshdXuQJbfRn0-XfdfZhVChBNWuHoXrqXg9kiAMWWj4cGsncd2PqRRugogEOMs-9iKSCiARl4Nrm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnOqxU5QC_nZgRkPHyeCNP-n4QIUB6fjPh83U0hwv1lFRVFgZq68P2O7iBaYhCunJ0JK7g-6hpKLuKFxq41JwiLj3YGXp3BxfjQ&lib=MXlEvNmo98u7JPl4wcD1WZM7W5hCA46zX"
-        )
-        .then(res => {
-          // handle success
-          this.speakersInfo = res.data.data
-        })
-        .catch(err => {
-          // handle error
-          console.log(err)
-        })
-        .then(() => {
-          // always executed
-        })
+      i: null,
+      speakersInfo: []
     }
   }
 }
